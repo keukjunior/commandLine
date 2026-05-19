684e522c20505535fcb7b50f0e54d03c64b25fc8
keukjunior <mapsjunior@hotmail.cm>
2026-05-19 18:43:48 +0000

    hello word1

--- src/main/java/sn/maps/commandeline/CommandeLineApplication.java
+++ src/main/java/sn/maps/commandeline/CommandeLineApplication.java
@@ -9,7 +9,7 @@ public class CommandeLineApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(CommandeLineApplication.class, args);
-        System.out.println("Hello World");
+        System.out.println("Hello");
     }
 
 }
