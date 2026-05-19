f193269ec5bcc1caff84e291400aeceb34df3672
keukjunior <mapsjunior@hotmail.cm>
2026-05-19 18:24:42 +0000

    iniatiation projet

---
+++ src/main/java/sn/maps/commandeline/CommandeLineApplication.java
@@ -0,0 +1,13 @@
+package sn.maps.commandeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandeLineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommandeLineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sn/maps/commandeline/CommandeLineApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.maps.commandeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandeLineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
